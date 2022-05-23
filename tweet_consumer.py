dd9aaf3329630cab56f6f9f3944546d7d6366d6c
Mansi-sigmoid <98111326+Mansi-sigmoid@users.noreply.github.com>
2022-05-23 17:11:52 +0530

    Update tweet_consumer.py

--- tweet_consumer.py
+++ tweet_consumer.py
@@ -21,4 +21,5 @@ def tweet_consumer():
     collection = client.get_database("New_Twitter").get_collection("info")
     for msg in consumer:
         collection.insert_one(msg.value)
+        print(msg.value)
 tweet_consumer()
