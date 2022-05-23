55f83d588cc9614cfbb4633c67eb0c961dc79ace
Mansi-sigmoid <98111326+Mansi-sigmoid@users.noreply.github.com>
2022-05-23 16:47:55 +0530

    Delete test directory

--- test/country.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Nominatim
-
-def get_country_name(place):
-    geolocator = Nominatim(user_agent="My-Application")
-    location = geolocator.geocode(place, addressdetails=True)
-    if location is None:
-        return ""
-    return location.raw['address']['country']
-
